a7647cde98f3c635a904d37e85969b95fed7e852
curtismager20 <curtismager@gmail.com>
2026-05-15 00:39:45 +0000

    Rebrand blog to match magertron.com (dark theme, Inter/DM Sans)

--- blog/src/consts.ts
+++ blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Magertron Blog';
+export const SITE_DESCRIPTION = 'Notes from building Kubernetes-native MCP infrastructure. Written by Curtis Mager.';
